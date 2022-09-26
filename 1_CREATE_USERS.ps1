feb3e0703e301e55fb40d7bc62df5da83ee167a9
[P]erSe3lan {N} <93418272+Persecure@users.noreply.github.com>
2022-09-26 20:25:10 +0800

    Update 1_CREATE_USERS.ps1

--- 1_CREATE_USERS.ps1
+++ 1_CREATE_USERS.ps1
@@ -3,15 +3,21 @@ $PASSWORD_FOR_USERS   = "Password1"
 $USER_FIRST_LAST_LIST = Get-Content .\names.txt
 # ------------------------------------------------------ #
 
+# --- Converts plain text or encrypted strings to secure strings --- #
 $password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+
+# --- Creates an Active Directory organizational unit --- #
 New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
 
+# --- A loop function that takes the first alphabet in the first name and combines with the last name--- #
 foreach ($n in $USER_FIRST_LAST_LIST) {
     $first = $n.Split(" ")[0].ToLower()
     $last = $n.Split(" ")[1].ToLower()
     $username = "$($first.Substring(0,1))$($last)".ToLower()
     Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
     
+# --- Creates an Active Directory user with the details provided--- #
+
     New-AdUser -AccountPassword $password `
                -GivenName $first `
                -Surname $last `
